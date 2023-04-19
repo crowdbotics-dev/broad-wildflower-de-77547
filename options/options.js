5df3e2cb766ddaf797a260afbf62daeee542c9cc
Crowdbotics <engineering@crowdbotics.com>
2023-04-19 05:42:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broad-wildflower-de-77547",
+  url: "https://broad-wildflower-de-77547.botics.co",
+  api: "https://broad-wildflower-de-77547.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
